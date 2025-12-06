7c9779af472f75501864f0309e1d77f6461ac84a
Eclipse <virgile.henry0211@gmail.com>
2025-12-06 13:59:49 +0100

    Cleaner algo, traded O(n log(k)) for O(nk), but k is usually small so its fine

--- src/lib.rs
+++ src/lib.rs
@@ -41,33 +41,46 @@ impl<const DIM: usize, T: Clone> VectorDatabase<DIM, T> {
 }
 
 impl<const DIM: usize, T> VectorDatabase<DIM, T> {
-    /// Find the n closest neighbor using linear search, with a O(n log(n)) complexity.
+    /// Find the n closest neighbor using linear search, with a O(n k) complexity,
+    /// with n the number of points, and k the max number of results.
     pub fn closest_neighbors(&self, initial_point: Vector<DIM>, max_results: usize) -> Vec<&T> {
-        let mut results: Vec<(f32, &T)> = Vec::with_capacity(max_results);
+        /// Structure to store result points as well as their distances to the target
+        struct DistancedData<'a, const DIM: usize, T> {
+            distance: f32,
+            data: &'a T,
+        }
+
+        let mut results: Vec<DistancedData<'_, DIM, T>> = Vec::with_capacity(max_results);
 
         /* Iterate over all data points and their vectors */
         for (vector, data_point) in self.data_points.iter() {
             /* Compute the distance for this data point to the target */
-            let distance = initial_point.euclidian_distance(vector);
+            let candidate = DistancedData {
+                distance: vector.euclidian_distance(&initial_point),
+                data: data_point,
+            };
 
-            /* Use binary search in the result to find where we should insert the new point, based on the distance */
-            match results.binary_search_by(|(d, _)| distance.total_cmp(d)) {
-                Ok(index) | Err(index) => {
-                    if results.len() < max_results {
-                        /* If the result vec is not yet full, we can simply insert at the given index */
-                        results.insert(index, (distance, data_point));
-                    } else {
-                        /* The index returned by the binary search is not the vec end, so we have a new best candidate */
-                        if index < results.len() {
-                            results.pop();
-                            results.insert(index, (distance, data_point));
-                        }
-                        /* Otherwise, this data point is worse than our curent best candidates, discard it */
-                    }
+            /* Linear search in the sorted result array to place the new candidate */
+            let mut insert_index = 0;
+            while let Some(result) = results.get(insert_index) {
+                if candidate.distance < result.distance {
+                    /* When the candidate is better than the current result at that index, it shall be inserted here */
+                    break;
+                } else {
+                    /* Otheriwse, keep looking */
+                    insert_index += 1;
                 }
+            }
+
+            /* Insert the candidate only if it is less than the max number of candidates */
+            if insert_index < max_results {
+                results.insert(insert_index, candidate);
             }
         }
 
-        results.into_iter().map(|(_, data)| data).collect()
+        results
+            .into_iter()
+            .map(|distanced| distanced.data)
+            .collect()
     }
 }
