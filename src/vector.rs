5a3e8bb57b3fb1072e54f3b4ddb2af9afcab37b4
Eclipse <virgile.henry0211@gmail.com>
2025-12-12 17:22:03 +0100

    Updated deserialization code

--- src/vector.rs
+++ src/vector.rs
@@ -40,9 +40,48 @@ impl<'de, const DIM: usize> serde::Deserialize<'de> for Vector<DIM> {
             fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
                 write!(formatter, "A {DIM}-dimension array of f32")
             }
+            fn visit_seq<A: serde::de::SeqAccess<'de>>(self, mut seq: A) -> Result<Self::Value, A::Error> {
+                let mut data = [0.0_f32; DIM];
+
+                for i in 0..DIM {
+                    let next = seq.next_element()?;
+                    data[i] = next.ok_or_else(|| serde::de::Error::invalid_length(i, &self))?;
+                }
+
+                Ok(data)
+            }
         }
 
         let data = deserializer.deserialize_seq(VectorVisitor::<DIM>)?;
         Ok(Vector { data })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn serialize_deserialize() {
+        /* Simple test */
+        let vector = Vector::new([0.0, 1.0, 2.0]);
+        let serialized = serde_json::to_string(&vector).unwrap();
+        let deserialized: Vector<3> = serde_json::from_str(&serialized).unwrap();
+
+        assert_eq!(vector, deserialized);
+
+        /* with f32 niche values (I wanted to do more funny stuff, but NaN and infs are 'null' in JSON) */
+        let vector = Vector::new([
+            0.0,               /* positive zero */
+            -0.0,              /* negative zero */
+            f32::EPSILON,      /* smallest increment > 1.0 */
+            f32::MIN_POSITIVE, /* smallest positive normal number */
+        ]);
+        let serialized = serde_json::to_string(&vector).unwrap();
+        let deserialized: Vector<4> = serde_json::from_str(&serialized).unwrap();
+
+        assert_eq!(vector, deserialized);
+
+        /* Maybe do more test with other formats ? binary ? */
     }
 }
